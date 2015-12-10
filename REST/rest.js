20c1dadabee0a4c15dd4099698dd4228d4aaeb8f
ccamposfuentes <carlos@ccamposfuentes.es>
2015-12-10 18:19:15 +0100

    consumir REST

---
+++ REST/rest.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+var https = require('http');
+
+var options = {
+    host: 'babole.si2.ninja',
+    path: '/SI2API/notifications',
+    method: 'GET',
+    headers: {'User-Agent': 'Prueba-Node-App'}
+};
+
+var req = https.get(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (datos_JSON) {
+  	//console.log(datos_JSON);
+  	var datos=JSON.parse(datos_JSON);
+  	console.log('type: ' + datos[0].type+ "\nTitulo: " + datos[0].alert +
+    "\nMensaje: " + datos[0].message);
+    });
+});
+req.end();
